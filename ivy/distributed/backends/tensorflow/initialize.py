3cfc7d92ec425cd90cd7bb8f7eeb88998c2417ac
Christopher Rae <raecd123@gmail.com>
2023-08-04 00:59:45 +0800

    Update initialize.py

--- ivy/distributed/backends/tensorflow/initialize.py
+++ ivy/distributed/backends/tensorflow/initialize.py
@@ -1,3 +1,6 @@
+import os
+
+
 import ivy.distributed as i_dist
 
 
@@ -5,7 +8,7 @@ def init_dist():
     context = i_dist.ParallelContext()
     context.reset_context()
     context.initilize()
-
+    os.environ["CUDA_VISIBLE_DEVICES"] = context.local_rank
     multi_machine = True if context.world_size / context.rank != 1 else False
     context.multi_machine = multi_machine
     context.default_group = i_dist.Group(range(context.world_size))
